aa568bb233e07452583489c150b7f70720f4d06e
olgaKhristo <olga84h@gmail.com>
2023-10-12 11:48:27 +0100

    testing complete
    Co-authored-by: jabz80 <jabz80@users.noreply.github.com>

--- server/__tests__/unit/controllers/orders.spec.js
+++ server/__tests__/unit/controllers/orders.spec.js
@@ -28,10 +28,79 @@ describe('orders controller', () => {
           .mockRejectedValue(new Error('Something happened to your db'))
           await ordersController.index(null, mockRes)
           expect(Orders.getAll).toHaveBeenCalledTimes(1)
-          expect(mockStatus).toHaveBeenCalledWith(500)
+          expect(mockStatus).toHaveBeenCalledWith(404)
           expect(mockJson).toHaveBeenCalledWith({ error: 'Something happened to your db' })
         })
 
+    it('should return an order when searched by id', async () => {
+        let testOrder = {        
+                order_id: 1,
+                user_id: 1,
+                book_id: 4,
+                collection_date: "2023-09-04",
+                order_reference: "1234"             
+          };
+    jest.spyOn(Orders, "getById").mockResolvedValue(new Orders(testOrder));
+    const mockReq = { params: { order_id: 1 } };
+    await ordersController.show(mockReq, mockRes);
+    expect(mockStatus).toHaveBeenCalledWith(200);
+    expect(mockJson).toHaveBeenCalledWith(new Orders(testOrder));
+    });
+
+    it('should return a new order when created new order', async () => {
+        let testOrder = {        
+                order_id: 1,
+                user_id: 1,
+                book_id: 4,
+                collection_date: "2023-09-04",
+                order_reference: "1234"             
+          };
+    jest.spyOn(Orders, "createOrder").mockResolvedValue(new Orders(testOrder));
+    const mockReq = { params: { order_id: 1 } };
+    await ordersController.createOrder(mockReq, mockRes);
+    expect(mockStatus).toHaveBeenCalledWith(201);
+    expect(mockJson).toHaveBeenCalledWith(new Orders(testOrder));
+
+    });
+
+    it('updates an order based on id', async () => {
+        const testOrder = {            
+                order_id: 1,
+                user_id: 1,
+                book_id: 4,
+                collection_date: "2020-09-04",
+                order_reference: "5555"
+              }   
+             
+        // Mock Users.prototype.updateUser
+        jest.spyOn(Orders, 'updateOrder')
+          .mockResolvedValue({ ...new Orders(testOrder), name: 'Updated Test Order' });
+      
+        // Create mockReq and mockRes
+        const mockReq = { params: { order_id: 1 }, body: testOrder };
+        const mockStatus = jest.fn();
+        const mockSend = jest.fn();    
+       
+      
+        // Call the controller method
+        await ordersController.updateOrder(mockReq, mockRes);
+      
+        // Assertions
+        
+        expect(Orders.updateOrder).toHaveBeenCalledTimes(1);
+        expect(mockStatus).toHaveBeenCalledWith(200);
+        expect(mockSend).toHaveBeenCalledWith({
+          data: new Orders({                      
+                order_id: 1,
+                user_id: 1,
+                book_id: 4,
+                collection_date: "2020-09-04",
+                order_reference: "5555"
+             
+          })
+        });
+      });
+      
 
 
 })
--- server/__tests__/unit/models/Users.spec.js
+++ server/__tests__/unit/models/Users.spec.js
@@ -10,4 +10,135 @@ describe('Users model', () => {
       expect(Users).toBeDefined()
     })
 
+    it('resolves all users', async () => {    
+      jest.spyOn(db, 'query').mockResolvedValueOnce({
+        rows: [{            
+            "user_id": 2,
+            "name": "jane smith",
+            "email": "janesmith@example.com",
+            "password": "qwerty123",
+            "access_lvl": 0         
+      
+      }],
+      });  
+    const users = await Users.getAll();
+    expect(users).toHaveLength(1);
+    expect(users[0]).toHaveProperty('user_id');
+  });
+
+  it('should return a user when search by email', async () => {
+    const email = 'janesmith@example.com'
+      jest.spyOn(db, 'query').mockResolvedValueOnce({
+        rows: [{      
+          user_id: 2,
+          name: "jane smith",
+          email: "janesmith@example.com",
+          password: "qwerty123",
+          access_lvl: 0
+        }]
+      });
+
+      const user = await Users.getOneByEmail(email);
+      expect(user).toBeDefined();
+      expect(user.user_id).toBe(2);
+      expect(user.email).toBe(email);
+      expect(user.name).toBe('jane smith');
+      });
+
+
+      it('should delete a user', async () => {
+        const user_id = 2
+        jest.spyOn(db, 'query').mockResolvedValueOnce({
+          rows: [{      
+            user_id: 2,
+            name: "jane smith",
+            email: "janesmith@example.com",
+            password: "qwerty123",
+            access_lvl: 0
+          }]
+        });
+        const deletedUser = await Users.destroy(user_id);
+        expect(deletedUser).toBeDefined();
+        expect(deletedUser.user_id).toBe(2);
+        expect(deletedUser.email).toBe('janesmith@example.com');
+        expect(deletedUser.name).toBe('jane smith');
+      });
+
+      it('should update a user', async () => {
+        const user_id = 2
+        const updateUser = {
+          name: "smith smith",
+          email: "janesmith@example.com",
+          password: "qwety5555",
+        }
+        jest.spyOn(db, 'query').mockResolvedValueOnce({
+          rows: [{      
+            user_id: 2, ...updateUser
+          }]
+        })
+        const updatedUser = await Users.updateUser(user_id, updateUser);
+        expect(updatedUser).toBeDefined();
+        expect(updatedUser.user_id).toBe(2);
+        expect(updatedUser.email).toBe(updateUser.email);
+        expect(updatedUser.name).toBe(updateUser.name);
+        expect(updatedUser.password).toBe(updateUser.password);
+      });
+            
+      it('should register a new user', async () => {
+        const newUser = {
+          user_id: 9,
+          name: "june smyth",
+          email: "jsmyth@example.com",
+          password: "qwer789",
+          access_lvl: 0
+        }
+        jest.spyOn(db, 'query').mockResolvedValueOnce({
+          rows: [{      
+            user_id: 9, ...newUser
+          }]
+        })
+        const registeredUser = await Users.register(newUser);
+        expect(registeredUser).toBeDefined();
+        expect(registeredUser.user_id).toBe(9);
+        expect(registeredUser.email).toBe(newUser.email);
+        expect(registeredUser.name).toBe(newUser.name);
+        expect(registeredUser.password).toBe(newUser.password);        
+      })
+
+it('should return a user when search by id', async () => {
+  const user_id = 3
+    jest.spyOn(db, 'query').mockResolvedValueOnce({
+      rows: [{   
+          "user_id": 3,
+          "name": "william allen",
+          "email": "william123@example.com",
+          "password": "pass1234",
+          "access_lvl": 0
+      }]
+    });
+    const user = await Users.getById(user_id);
+    expect(user).toBeDefined();
+    expect(user.user_id).toBe(3);
+    expect(user.email).toBe('william123@example.com');
+    expect(user.name).toBe('william allen');   
+
+})
+
+it('should throw an error when no user is found', async () => {
+  // Define a sample user ID for the test
+  const nonExistentUserId = 456;
+  // Mock the db.query function to return an empty response (no matching rows)
+  jest.spyOn(db, 'query').mockResolvedValueOnce({ rows: [] });
+  // Use async/await with try-catch to catch the error
+  try {
+    await Users.getById(nonExistentUserId);
+    // If no error is thrown, fail the test
+    fail('Expected an error to be thrown');
+  } catch (error) {
+    // Assert that the error message matches your expectation
+    expect(error.message).toBe('No User Found');
+  }
+});
+
+
 });
